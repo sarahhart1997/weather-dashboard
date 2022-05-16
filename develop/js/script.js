08985c846d223b1b5981ee8b968440bea840b558
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-16 13:58:00 -0400

    find city name

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,12 +1,27 @@
 //Load the important things
 var searchBtn = document.getElementById('search-btn');
+var location = document.getElementById('location-input');
 
 // Event Listeners/Main Navigation
 
 // Define the current date
 
-// Get the current weather
+// Gets the city name from search box
+var getLocation = function(){
+    var locationName = location.value.trim();
+    if(locationName) {
+        getWeather(locationName);
+        // clear search
+        locationName.value="";
+    } else {
+        window.alert("Please search for a city");
+    }
+}
 
+// Get the current weather
+var getWeather = function(locationName) {
+    
+}
 // Display Weather
         //Current Weather
 
